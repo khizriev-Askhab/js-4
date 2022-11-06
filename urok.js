bbb269e53f2b7f30dac7affc10da0039aa4cbb0f
khizriev <askhab.askhabb.khizriev@mail.ru>
2022-11-07 00:18:38 +0300

    good

---
+++ urok.js
@@ -0,0 +1,48 @@
+
+                                     //1 задания
+
+function summ( a ){ return a % 2==0 ? `${a} четное число `: `${a} не четное число`}
+    console.log(summ(3333));
+    console.log(summ(2344));
+
+
+                                // 2 задания
+            
+
+
+    function more(a, b, c) {
+        if ((a >= b) && (a >= c)) { 
+      return a;
+        } else if ((b >= a) && (b >= c)) {
+      return b;
+        } else {
+      return c;
+ }
+ }
+                                
+   console.log(more(1,3,2));
+                 
+
+                            // 3 задания
+                
+
+function x (a,f ){
+    const result = a * f;
+    console.log(result)
+
+}
+x(5400,0.10);
+
+
+                        // 4 задания
+
+
+
+function f (h,g){
+let h = lorem
+let g = 6
+if ( h < length){
+    console.log(`ошибка,слишком маленький текст`)
+}
+else
+}
